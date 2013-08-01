e02b2e9a9fccbe51dab7a6129098fba54327b267
River Marmorstein <Rivermarmorstein@gmail.com>
2013-08-01 06:09:22 -0400

    update to 1.6, changed config, init on deathpay feature

--- src/me/Pluto1099/GrimReaper/GrimReaper.java
+++ src/me/Pluto1099/GrimReaper/GrimReaper.java
@@ -2,18 +2,27 @@ package me.Pluto1099.GrimReaper;
 
 import java.util.logging.Logger;
 
+import net.milkbowl.vault.economy.Economy;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GrimReaper extends JavaPlugin {
+public class GrimReaper extends JavaPlugin implements Listener {
 	String prefix = "[Reaper] ";
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static GrimReaper plugin;
+	boolean deathkills = this.getConfig().getBoolean("DeathPay");
+	int amt = this.getConfig().getInt("DeathPayAmt");
+	public static Economy economy = null;
 	
 	@Override
 	public void onDisable() {
@@ -26,6 +35,15 @@ public class GrimReaper extends JavaPlugin {
 	public void onEnable () {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() + "I have risen!");
+		getServer().getPluginManager().registerEvents(this, this);
+	    loadConf();
+	    if (economy == null) {
+	    	deathkills = false;
+	    }
+	        }
+	private void loadConf() {	
+		getConfig().addDefault("deathpay", false);
+		getConfig().addDefault("deathpay.amount", 25);
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
@@ -50,4 +68,24 @@ public class GrimReaper extends JavaPlugin {
 		} return true;
 	}
 	
+	@EventHandler
+	public void onKill(PlayerDeathEvent e) {
+		String killed = e.getEntity().getName();
+		String killer = e.getEntity().getKiller().getName();
+		Player award = Bukkit.getServer().getPlayer(killer);
+		if (deathkills) {
+			award.sendMessage(ChatColor.GRAY + prefix + ChatColor.WHITE + "You have been awarded " + ChatColor.GREEN + amt + ChatColor.WHITE + " For killing " + killed);
+			economy.depositPlayer(killer, amt);
+		}
+	}
+	
+    private boolean setupEconomy()
+    {
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+
+        return (economy != null);
+    }
 }
